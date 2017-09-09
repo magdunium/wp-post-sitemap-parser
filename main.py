078464bf64b5d6ea12716e0fdb524d4867f039c6
Magdum <magdunium@gmail.com>
2017-09-09 16:34:26 +0200

    Add if/else statement, which prints out http status if it's different than 200.

--- main.py
+++ main.py
@@ -3,11 +3,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-urls = ['http://yourdomain1.pl/post-sitemap.xml','http://yourdomain2.pl/post-sitemap.xml']
+urls = ['http://yourdomain.pl/post-sitemap.xml','http://yourdomain.pl/post-sitemap.xml']
+
 
 for url in urls:
-    html = requests.get(url).text
-    bs = BeautifulSoup(html, 'html.parser')
-    possible_links = bs.find_all('loc')
-    for link in possible_links:
-        print link.text
+    html = requests.get(url)
+
+    if html.status_code != 200:
+        print url, html.status_code
+    else:
+        bs = BeautifulSoup(html.text, 'html.parser')
+        possible_links = bs.find_all('loc')
+        for link in possible_links:
+            print link.text
